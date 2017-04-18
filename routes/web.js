817c2e27c7705a71229cd185f22aec1dbeb23316
赵骥 <zhaoji02@dianping.com>
2017-04-18 15:38:11 +0800

    fe change

--- routes/web.js
+++ routes/web.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET mobile page. */
-router.get('/web', function(req, res, next) {
+/* GET web page. */
+router.get('/index', function(req, res, next) {
   res.render('web/index', {
     path:"web/index"
   });
